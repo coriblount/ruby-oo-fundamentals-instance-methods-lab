1f2ea7ec4c3c1e1df1bb88cbe479c1a0ea325ca3
Cori Blount <heycorihey@icloud.com>
2021-02-23 14:21:14 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,3 +13,26 @@ end
 adele_goldberg = Person.new
 
 alan_kay = Person.new
+
+class Dog
+    def name=(dog_name)
+        this_dogs_name = dog_name
+    end
+
+    def name
+        this_dogs_name
+    end
+end 
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  
+    def sit
+      puts "The Dog is sitting"
+    end
+  end
