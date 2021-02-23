9f84fa26b192b7ec9035b5e73a9c4eb0bcc97f32
Cori Blount <heycorihey@icloud.com>
2021-02-23 14:24:37 -0600

    commit tree 1e1d3bbb45a4d63b56e12df770f83d585caa6136

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,3 +36,20 @@ class Dog
       puts "The Dog is sitting"
     end
   end
+
+  class Dog
+
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
+   
